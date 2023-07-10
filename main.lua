ace36e3765e0e5238172d2c412a8b30b4e2b250b
Vadim Brodsky <VadimBrodsky@users.noreply.github.com>
2023-07-10 18:45:09 -0400

    Set default filter to nearest neighbour

--- main.lua
+++ main.lua
@@ -7,6 +7,8 @@ VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
 function love.load()
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+
   push:setupScreen(
     VIRTUAL_WIDTH,
     VIRTUAL_HEIGHT,
