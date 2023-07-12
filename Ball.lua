a3efa097a96c50325a6e5f8a7c1d099b20148625
Vadim Brodsky <VadimBrodsky@users.noreply.github.com>
2023-07-12 09:24:38 -0400

    Add Ball:collides AABB function

--- Ball.lua
+++ Ball.lua
@@ -26,3 +26,21 @@ end
 function Ball:render()
 	love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
 end
+
+-- AABB collision algorithm
+function Ball:collides(paddle)
+	-- check to see if the left edge of either is farther to the right
+	-- than the right edge of the other
+	if self.x > paddle.x + paddle.width or paddle.x > self.x + self.width then
+		return false
+	end
+
+	--  check to see if the bottom edge of either is higher than the top
+	-- edge of the other
+	if self.y > paddle.y + paddle.height or paddle.y > self.y + self.height then
+		return false
+	end
+
+	-- if the above aren't true, they're overlapping
+	return true
+end
